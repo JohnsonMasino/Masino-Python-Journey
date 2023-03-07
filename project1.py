c803d1026066812da610e535ca09aee48a2576e5
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-07 03:15:45 -0800

    Project 1

---
+++ project1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+print("String Manipulation Exercise")
+print("String Concatenation is done with '+' Sign")
+print("e.g print('Hello' + 'Masino')")
+print("New lines can be created with a backslash and n")
+print("------------------------------------------------")
+print("Code developed by Masino")
