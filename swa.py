2843543c9e317f5c79dc5ec3deb1d2679b315dd7
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-08 06:55:47 -0800

    Swapping two numbers

---
+++ swa.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+a = input("Enter A value here ")
+b = input("Enter B value here ")
+print("The value of A is: " + a)
+print("The value of B is: " + b)
+swa = a
+a = b
+b = swa
+print("To interchange these numbers, we have that:")
+print("The value of A is: " + a)
+print("The value of B is: " + b)
+print("Tanks for this practice")
