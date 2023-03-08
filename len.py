01f2c608d7bb3617e58fb62a9d6ac3a401681aeb
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-08 06:44:26 -0800

    Length of string

---
+++ len.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+name = input("Hi dear!\nWhat is your name...? ")
+print(name + " Welcome here dear")
+length = len(name)
+print("Well, the length of your name is:")
+print(length)
+print("\nCode devloped by Masino")
