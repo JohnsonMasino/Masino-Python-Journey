50f575e84679fdec453927de6c4e367560ff67c8
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-09 06:35:45 -0800

    Adding input of ints

---
+++ input_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+num = input("Enter two numbers here ")
+first = num[0]
+second = num[1]
+summ = (int(first) + int(second))
+print("The numbers you entered are " + num)
+print("The sum of them both is: " + str(summ))
+print("\nCode developed by Masino")
