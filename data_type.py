c648d988552805ddc7448137f156f3b9b7f46c77
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-09 03:25:34 -0800

    Data_types

---
+++ data_type.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+binary = 0b10
+octal = 0o123
+hexadecimal = 0x33
+Float = 23.44
+string = "Masino"
+boolean = True
+boolean1 = False
+
+print(type(binary))
+print(type(octal))
+print(type(hexadecimal))
+print(type(Float))
+print(type(string))
+print(type(boolean))
+print(type(boolean1))
+print("\nCode developed by Masino")
