e1ce6eac50d4fb1affd52bf63d9b9f92c09ca7b2
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-09 03:58:03 -0800

    Concatenate data_types

---
+++ con_cat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+string = input("What is your name ?\n")
+integer = input("What is your age ?\n")
+floating = input("What is your credit score in $ ?\n")
+print("\nWelcome here " + string)
+print("We got that your age is " + integer + " and your CS is " + floating)
+print("\nCode developed by Masino")
