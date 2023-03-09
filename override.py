bc70c5d5f3fd001665b904ad216ae09a446caa86
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-09 03:08:29 -0800

    To override a special character

---
+++ override.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+print("To override a special character like '\"', we add a \ before it ")
+print("CHeckout this line of code:")
+print("My name is \"Masino\"")
+print("What I did is My name is \\\"Masino\\\"")
+print("Code developed by Masino")
