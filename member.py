4082252e45835197bc872a433512ce16f3da84c4
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-11 06:09:37 -0800

    Are you a member...?

---
+++ member.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+NAME = "Johnson Masino"
+numbers = [1, 4, 8, 0, 0, -9]
+print("8" in numbers)
+print("-8" not in numbers)
+print("j" in NAME)
+print("son" not in NAME)
+print(NAME[6] == "n")
+print(NAME[0] == "o")
+print("\nCode developed by Masino")
