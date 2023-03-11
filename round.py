25517e872af38f43fe174bcc43bc012fce01f4ff
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-11 07:30:01 -0800

    Rounding up my digits here

---
+++ round.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+number = input("Enter a floating number:  ")
+rounding = input("I should round it up to what please ?  ")
+print("Okay!\nWorking that out is a second...\nGot it!")
+num = float(number)
+roun = int(rounding)
+print(round(num, roun))
+print("Program developed by Masino")
+
