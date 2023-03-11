8cdf76ff76722561f6aa52dd7f0eb7493be0d943
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-11 07:38:13 -0800

    You want to know little about me?

---
+++ concat_string.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+name = "Johnson"
+age = "23"
+height = "1.8m"
+print(f"Hi! My name is {name} and I am {age} years old.\nI am also {height} tall.\nThank you for listening to me.")
+print("\nCode developed by Masino")
