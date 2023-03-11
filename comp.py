c4e5a3e95fa16b7126facb3300685004685e72d5
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-10 17:43:29 -0800

    Assignment operations

---
+++ comp.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+a, b, c = 2, 0, 9
+print("a, b, c = 2, 0, 9")
+print("a == 2 = " + str(a == 2))
+print("a > b = " + str(a > b)) 
+print("a += 2 = " + str(a + 2))
+print("(c + 3) != 12 = " + str((c + 3) != 12))
+print("b = c = " + str(b == c))
+print("(a + b + 7) == c = " + str((a + b + 7) == c) + "\n")
+print("Code developed by Masino")
