728682f52114adafb327c41292193f161e3d0f8c
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-10 18:12:14 -0800

    Bitwise Operators Chanting

---
+++ bitwise.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+a, b = 3, 5
+print(a & b)
+print(~b)
+print(~a)
+print(a ^ b)
+print("printing the value of a first " + str(a) + " " + "and then the next " + str(a / b))
+# print(a / b)
+print(a >> b)
+print(b >> a)
+print(a << b)
+print(a | b)
+print ("\nCode developed by Masino")
