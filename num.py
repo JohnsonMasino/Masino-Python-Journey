dcf81dbe7d54d5bb972501fb6824bc5e93c2f521
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-12 07:56:51 -0700

    One to Four Only

---
+++ num.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+inp = int(input("Enter a number here:  "))
+if inp == 1:
+    print("You just entered 1")
+elif inp == 2:
+    print("You entered 2")
+elif inp == 3:
+    print("Yoiu entered 3 here")
+elif inp == 4:
+    print("You entered 4")
+else:
+    print("Not recognised")
+print("Thanks for trusting us")    
+print("\nCode developed by Masino")    
