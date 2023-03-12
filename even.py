2f8dfe8a81fae5c5261319bf3441ff4da8ecd4c5
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-12 06:20:50 -0700

    Odd me and Even me

---
+++ even.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+number = int(input("Enter a number here to verify:  "))
+num = (number % 2)
+#print(num)
+if (num == 0):
+    print("This number is an even number")
+else:
+    print("This number is an odd number")
+print("Thanks for your time...")
+print("\nCode developed by Masino")
