bbda3e69fd268bbf3ceae19f6e84a6b8eeac6cc7
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-13 10:56:55 -0700

    modified

--- nested_if.py
+++ nested_if.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 print("Welcome to our snooker camp champ!")
 age = int(input("Enter age here:  "))
-#sal = int(input("Enter 1 if employed and 0 if unemployed")
-if age < 18:
-    print("Sorry!\nYou can not play the snooker")
-#    sal = int(input("Enter 1 if you are employed and 0 if unemployed")
-#   if sal == 1:
- #      print("Pay to play")
-  # else:
-   #    print("Dont pay to play")
+if age >= 18:
+    print("You can play snooker here!")
 else:
-    print("Good!\nYou can play the snooker with us")
+    print("Sorry!\nYou can not play here.")
+print("Code developed by Maino")
