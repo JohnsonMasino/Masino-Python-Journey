47eb2fe0c98cb5424dbe606c340a837cf0602ba5
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-13 11:24:12 -0700

    You are rich

--- nested_if.py
+++ nested_if.py
@@ -5,7 +5,16 @@ if age >= 18:
     print("You can play snooker here!")
     sal = int(input("Enter 1 if employed and 0 if not  "))
     if sal == 1:
-        print("You have to pay $10 to play\nThank you.")
+        print("You have to pay some amount to play snooker here.")
+        amount = int(input("Enter your monthly salary in USD  "))
+        if amount <= 1000:
+            print("You have to pay USD10 to play here\nThank you!")
+        elif amount > 1000 and amount <= 5000:
+            print("You have to pay USD25 to play snooker here.\nThank you.")
+        elif amount > 5000 and amount <= 10000:
+            print("Your fee is USD50.\nThank you!")
+        else:
+            print("Wow you are rich$\nYour fee is USD100.\nThank you!")
     else:
         print("You can play for free\nThank you.")
 else:
