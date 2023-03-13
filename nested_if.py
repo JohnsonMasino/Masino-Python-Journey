c7206d13c0ea743d6c08134a83c3d057291a8077
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-13 11:05:58 -0700

    Updated Snooker Bot

--- nested_if.py
+++ nested_if.py
@@ -3,6 +3,12 @@ print("Welcome to our snooker camp champ!")
 age = int(input("Enter age here:  "))
 if age >= 18:
     print("You can play snooker here!")
+    sal = int(input("Enter your salary here in USD  "))
+    if sal <= 1000:
+        print("You can play for free")
+    else:
+        print("You have to pay $10 to play\nThank you.")
 else:
     print("Sorry!\nYou can not play here.")
-print("Code developed by Maino")
+print("Thanks for choosing us")    
+print("\nCode developed by Maino")
