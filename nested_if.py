f1f0e9716fa8be1c9393c80b6a82390b8606efcc
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-13 11:10:38 -0700

    Are you employed ?

--- nested_if.py
+++ nested_if.py
@@ -3,12 +3,12 @@ print("Welcome to our snooker camp champ!")
 age = int(input("Enter age here:  "))
 if age >= 18:
     print("You can play snooker here!")
-    sal = int(input("Enter your salary here in USD  "))
-    if sal <= 1000:
-        print("You can play for free")
-    else:
+    sal = int(input("Enter 1 if employed and 0 if not  "))
+    if sal == 1:
         print("You have to pay $10 to play\nThank you.")
+    else:
+        print("You can play for free\nThank you.")
 else:
     print("Sorry!\nYou can not play here.")
-print("Thanks for choosing us")    
+print("Have fun!!!")    
 print("\nCode developed by Maino")
