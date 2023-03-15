c5ebbea6b760365b1ba8fbdb959fbc7f65339db9
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-15 13:50:43 -0700

    Add me to the list

---
+++ sec.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+print("Code execution starts here...")
+lists = ['Coming', 23, 'John', 67, '90', 'Obinna', 33, 'Masino', 100]
+print(lists)
+print("Adding 'Home' to the last part:")
+lists.append("Home")
+print(lists)
+print("Adding '13' to third position:")
+lists.insert(2, 13)
+print(lists)
+print("End of code execution!")
+print("\nCode developed by Masino")
