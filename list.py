9a20e1df7d41ba9333d58875209dd4d31d10910a
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-15 11:05:00 -0700

    access me

---
+++ list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+print("Welcome!\nWorkings starts here.")
+lists = [1, 0, 34, 44, -9, -1]
+print(lists)
+print("The third element is: " + str(lists[2]))
+print("\nCode developed by Masino")
