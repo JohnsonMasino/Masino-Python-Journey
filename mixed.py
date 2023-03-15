b9fb6bf1c177d28b3b9c937bed073ba293d1e016
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-15 11:15:57 -0700

    Positive and Negative Finder

---
+++ mixed.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+print("Welcome!\nWorking starts here...")
+lists = ['Masino', 78, 'Johnny', 23.43, 'TRUE', 13, 'Obi']
+print(lists)
+print("The third element is: " + str(lists[2]))
+print("Counting from the end, ie -5, we still have: " + lists[-5])
+print("End of execution!")
+print("\nCode devloped by Masino")
