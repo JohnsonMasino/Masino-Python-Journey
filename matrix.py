72cf85f65a68c4dbb45f8db3d18c7a416b5010f0
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-17 12:37:25 -0700

    Find me in this Matrix

--- matrix.py
+++ matrix.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
 print("Welcome to the matrix world...")
-lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+lists = [[1, 2, 3, 4], [7, 4, 5, 6], [7, 8, 9, 3], [5, 0, 1, 9], [2, 0, 1, 3]]
 print("Below is our matrix stand for today:")
 print(lists[0])
 print(lists[1])
 print(lists[2])
+print(lists[3])
+print(lists[4])
 number = input("Enter row and colum(separate with a comma) of a number to access it: ")
 sort_number = number.split(",")
 num00 = sort_number[0]
@@ -13,6 +15,6 @@ num01 = sort_number[1]
 num0 = int(num00)
 num1 = int(num01)
 matrix = lists[num0 - 1][num1 - 1]
-print(matrix)
+print("The element in this locations is: " + str(matrix))
 #print("Tha value in that position is: " + str(matrix))
 print("\nCode developed by Masino")
