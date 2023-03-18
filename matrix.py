5967d79989e57c2817dd55f1c52739d3543d2b35
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-18 07:40:44 -0700

    Edited

--- matrix.py
+++ matrix.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 
+lists = [[1, 2, 3, 4], [7, 4, 5, 6], [7, 8, 9, 5], [5, 0, 1, 9], [2, 0, 1, 3]]
 print("Welcome to the matrix world...")
-lists = [[1, 2, 3, 4], [7, 4, 5, 6], [7, 8, 9, 3], [5, 0, 1, 9], [2, 0, 1, 3]]
+lists0 = [1, 2, 3, 4]
+lists1 = [7, 4, 5, 6]
+lists2 = [7, 8, 9, 5]
+lists3 = [5, 0, 1, 9]
+lists4 = [2, 0, 1, 3]
 print("Below is our matrix stand for today:")
-print(lists[0])
-print(lists[1])
-print(lists[2])
-print(lists[3])
-print(lists[4])
+print(f"{lists0}\n{lists1}\n{lists2}\n{lists3}\n{lists4}")
 number = input("Enter row and colum(separate with a comma) of a number to access it: ")
 sort_number = number.split(",")
-num00 = sort_number[0]
-num01 = sort_number[1]
-num0 = int(num00)
-num1 = int(num01)
+num0 = int(sort_number[0])
+num1 = int(sort_number[1])
 matrix = lists[num0 - 1][num1 - 1]
 print("The element in this locations is: " + str(matrix))
-#print("Tha value in that position is: " + str(matrix))
+
+
+
 print("\nCode developed by Masino")
