d654f9ff3a85ab04acf9485e153bbdd326eef08a
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-19 00:13:03 -0700

    Your miles to kilometers

---
+++ miles.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+miles = int(input("Welcome to miles > kilometers converter!\nEnter the miles(number only) you want to convert to kilometers:  "))
+kilometer = miles * 1.60934
+print(f"{miles} miles is equivalent to {kilometer} kilometers")
+print("\nCode developed by Masino")
