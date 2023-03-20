59f4cf011796476b45deb10911ee1f69777d9221
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-20 06:06:20 -0700

    Round to two

---
+++ fl.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+num = float(input("Hi there!\nEnter a float number here:  "))
+num0 = round(num, 2)
+print(f"This number in two decimal places is: {num0}")
+print("\nCode developed by Masino")
