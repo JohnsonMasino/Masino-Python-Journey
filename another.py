29b3428e0161f0a318b32a0a89b48a68754280e8
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-20 05:40:23 -0700

    No space exe

---
+++ another.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+print("Welcome here")
+for i in range(0, 10):
+    print("I = ", i)
+for i in(1, 2, 3, 4, 9, 10):
+    print("I = ", i)
+print("\nCode developed by Masino")    
