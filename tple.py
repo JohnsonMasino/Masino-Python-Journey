64757e0a2c9d35b24f981f69e1d8d95344160fe6
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-23 10:31:15 -0700

    Tuple Code

---
+++ tple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+tple1 = (10, 'Masino', 34, True, -8, 0, 'Obinna', False, -1)
+print("First element: " + str(tple1[0]))
+print("Last element using negative indexing is: " + str(tple1[-1]))
+print("The datatype of this tuple is: " + str(type(tple1)))
+print("Counting from the fourth element to the sixt:\n" + str(tple1[3:6]))
+print("The length of this tuple is: " + str(len(tple1)))
+
+
+print("\nCode developed by Masino")
