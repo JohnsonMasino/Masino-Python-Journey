c39bca86f1878c02bf026844c595fa061ae0b204
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-24 14:00:52 -0700

    Xmas Tree

---
+++ tree.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+num = int(input("Hello!\nWelcome to screen tree printer.\nWhat is the height of the tree you want to print ?  "))
+print("Okay, Got it!")
+top_space = num - 1
+hashes = 1
+stump_spaces = num - 1
+while num != 0:
+    for i in range(top_space):
+        print(' ', end="")
+    for i in range(hashes):
+        print('#', end="")
+    print("\n")
+    top_space -= 1
+    hashes += 2
+    num -= 1
+for i in range(stump_spaces):
+    print(' ', end="")
+print("#")
+print("\nCode developed by Masino")
