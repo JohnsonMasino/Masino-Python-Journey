dc83a63a0264dd1a4c2e1555af0c5b9df15a081f
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-25 12:18:34 -0700

    Set operations in Python

---
+++ sett.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+set1 = {'Masino', 'Kings', 'Real-emmy', 'Obyno'}
+set2 = {'Obum', 'Ebube', 'Masino'}
+print(set1)
+print(set2)
+print(set1.union(set2))
+print(set1 | set2)
+set1.update(set2)
+print(set1)
+print("\nCode developed by Masino")
