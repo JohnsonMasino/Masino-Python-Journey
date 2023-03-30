749a42d3668db18ac0792318f41152d855ca191c
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-30 14:31:03 -0700

    More for loop

---
+++ for_name.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+list_of_names = ['Masino', 'Obinna', 'Johnson', 'Stephen']
+for name in list_of_names:
+    print(name)
+    if name == 'Johnson':
+        print(f"{name} is the first name of this owner")
+else:
+    print("End of operation")
+print("\nCode developed by Masino")    
