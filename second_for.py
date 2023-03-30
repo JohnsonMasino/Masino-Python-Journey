043b1fe617d688b8b8a124f786fb925266ba7838
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-30 14:17:48 -0700

    More for loop

---
+++ second_for.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+name = 'Johnson Masino'
+name_list = []
+print("The name output is shown below:")
+for letter in name:
+    print(letter, end="")
+    name_list.append(letter)
+#    print(name_list)
+else:
+    print("\nThe name in a list is: " + str(name_list))
+    print("End of letters")
+print("\nCode developed by Masino")    
