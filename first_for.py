721cfc0de51f0f0674713dd0aa67f4d83cff065e
JohnsonMasino <johnsonmasino@gmail.com>
2023-03-30 13:59:42 -0700

    For loop

---
+++ first_for.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+names = {'Jenny', 'Masino', 'Obynno', 'Yemi', 'Osibanjo'}
+for name in names:
+    print(name)
+else:
+    print("Completed all names")
+print("\nCode developed by Masino")    
