18373b4f3cbd6b529d04c54cfa82f0c87daf365e
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-02 00:27:39 -0700

    More Maths

---
+++ my_math2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import math
+
+print("Logarithm of 20 is: ", math.log(20))
+print("Log(1000, 10) is: ", math.log(1000, 10))
+print("Log10(1000) is: ", math.log10(1000))
+print("\nCode developed by Masino")
