b3cb3c6404a24ccd3f7485e36b23c39c5096502c
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-02 00:23:19 -0700

    More maths

---
+++ my_math1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import math
+
+print("This value is float(4.2) but I will return int: ", math.trunc(4.2))
+print("Pow(3,2) = ", math.pow(3,2))
+print("Square root of 36 is: ", math.sqrt(36))
+print("Special value of e is: ", math.e)
+print("Special value of pi is: ", math.pi)
+#print("Sum of 3,3 is: ", math.sum(3,3))
+print("\nCode developed by Masino")
