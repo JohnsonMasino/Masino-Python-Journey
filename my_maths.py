d10000d9fea196378d14fe95ee2a90ac1efca56a
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-02 00:07:01 -0700

    Maths Functions

---
+++ my_maths.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import math
+
+print("ceil of 4.4 = ", math.ceil(4.4))
+print("floor of 4.4 = ", math.floor(4.4))
+print("fabs of -4.4 = ", math.fabs(-4.4))
+print("Factorial of 4 is: ", math.factorial(4))
+print("fmod of 5,4 is: ", math.fmod(5,4))
+print("trunc of 4.2 is: ", math.trunc(4.2))
+print("\nCode developed by Masino")
